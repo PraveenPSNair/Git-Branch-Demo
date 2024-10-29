cc36e0228ae9fa911e6cd55f1ea5ff20b2b566a4
unknown <praveenps@live.com>
2024-10-29 15:08:09 +0530

    Initial Commit

---
+++ src/test/java/com/example/Git_Branch_Demo/GitBranchDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Git_Branch_Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitBranchDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
